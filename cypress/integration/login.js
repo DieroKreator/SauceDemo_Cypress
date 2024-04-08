bcd015e263ba8b52e084ae10b74ce263a1ffe901
DieroKreator <qtechwired@gmail.com>
2024-04-08 17:15:56 -0300

    Added tests for the logIn function

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -1,14 +1,21 @@
 describe('Authentication', () => {
     beforeEach(() => {
         cy.visit('https://www.saucedemo.com/')
+    })
 
+    it('Verify that users can successfully login to the website', () => {
         cy.get('#user-name').type("standard_user")
         cy.get('#password').type("secret_sauce")
         cy.get('#login-button').click()
+
+        cy.get('.title').should('have.text', 'Products')
     })
 
-    it('should be able to login successfully', () => {
-        cy.get('.title').should('have.text', 'Products')
-    
+    it('Verify that users cannot login with invalid credentials', () => {
+        cy.get('#user-name').type("standard")
+        cy.get('#password').type("sec_sauce")
+        cy.get('#login-button').click()
+
+        cy.get('h3').should('have.text', 'Epic sadface: Username and password do not match any user in this service')
     })
 })
